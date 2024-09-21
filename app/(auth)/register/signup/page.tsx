5af52f45f7acf87e72b240a267488a49c3582f3a
Arjun Yadav <arjun149133@gmail.com>
2024-09-21 20:35:06 +0530

    .env remove

--- app/(auth)/register/signup/page.tsx
+++ app/(auth)/register/signup/page.tsx
@@ -1,5 +1,11 @@
+"use client";
+import { Button } from "@/components/ui/button";
+import { signInWithGoogle } from "@/lib/auth-actions";
+
 const SignupPage = () => {
-  return <div>hello peter parker</div>;
+  return (
+    <Button onClick={() => signInWithGoogle()}>Sign Up With Google</Button>
+  );
 };
 
 export default SignupPage;
